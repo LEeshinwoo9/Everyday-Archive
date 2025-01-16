111fb2bcc2c166f34e2bc163706b9023ea4f2e7e
LEeshinwoo9 <0609ted@naver.com>
2025-01-16 09:25:05 +0900

    Day 6: Java 연습코드

--- src/day6c/ExceptEx1.java
+++ src/day6c/ExceptEx1.java
@@ -1,4 +1,39 @@
 package day6c;
+import java.io.*;
+class B{
+    void run() throws IOException, FileNotFoundException{
+        BufferedReader bReader = null;
+        String input = null;
+        bReader = new BufferedReader(new FileReader("out.txt"));
+        input = bReader.readLine();
+        System.out.println(input);
+    }
+
+}
+class C {
+    void run() throws IOException, FileNotFoundException{
+        B b = new B();
+        b.run();
+
+    }
+
+}
+
+
+
+
 
 public class ExceptEx1 {
+
+    public static void main(String[] args) {
+        C c = new C();
+        try {
+            c.run();
+        }catch (FileNotFoundException e ){
+            System.out.println("out.txt 파일은 설정 파일입니다. 이 파일이 프로젝트 루트 디렉토리에 존재해야 합니다.");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/day6c/PolymorphismEx1.java
+++ src/day6c/PolymorphismEx1.java
@@ -1,4 +1,40 @@
 package day6c;
 
+interface I2{
+    public String A();
+}
+interface  I3{
+    public String B();
+}
+
+class D implements I2,I3{
+
+    @Override
+    public String A() {
+        return "A";
+    }
+
+    @Override
+    public String B() {
+        return "B";
+    }
+}
+
 public class PolymorphismEx1 {
+
+    public static void main(String[] args) {
+        D obj = new D();
+        I2 obj1 = new D();
+        I3 obj2 = new D();
+
+        obj.A();
+        obj.B();
+        obj1.A();
+        // obj1.B();
+        //obj2.A();
+        obj2.B();
+
+
+    }
+
 }
